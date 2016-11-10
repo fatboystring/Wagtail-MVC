4ca1c0bf5e950ab9710d8a76aa788a5f22641395
Dan Stringer <dan.stringer1983@googlemail.com>
2016-11-10 08:14:02 +0000

    Add test stubs for Model Mixin behaviour

--- wagtail_mvc/tests.py
+++ wagtail_mvc/tests.py
@@ -31,3 +31,44 @@ class WagtailMvcViewWrapperTestCase(TestCase):
         """
         self.instance.get_view_restrictions()
         self.page.get_view_restrictions.assert_called_with()
+
+
+class WagtailMvcMixinTestCase(TestCase):
+    """
+    Tests the WagtailMvcMixin
+    """
+    def test_calls_serve_if_wagtail_url_conf_not_defined(self):
+        """
+        The serve method should still be called if the wagtail_url_conf attribute is not defined
+        """
+        pass
+
+    def test_resolve_view_resolves_view(self):
+        """
+        The resolve_view method should return the correct data
+        """
+        pass
+
+    def test_page_in_view_kwargs(self):
+        """
+        The resolve_view method should add the page instance to the view kwargs
+        """
+        pass
+
+    def test_resolve_view_raises_404(self):
+        """
+        The resolve_view method should raise a Resolver404 exception
+        """
+        pass
+
+    def test_url_config_used_to_serve_actual_page(self):
+        """
+        The defined url config should be used to serve the page when a full url is matched
+        """
+        pass
+
+    def test_url_config_used_to_serve_sub_page(self):
+        """
+        The defined url config should be used to serve a sub page when a partial url is matched
+        """
+        pass
